a48ede32112163c9903dd5a3165ca002af7a50d5
duygusen <seenduygu@gmail.com>
2024-08-25 20:27:16 +0300

    Changes were made to the login spec file. Methods and steps were edited.

--- specs/login.spec
+++ specs/login.spec
@@ -1,4 +1,14 @@
 # login.spec
 
-## Login Scenario
-* OpenTrendyol
+## Setup
+* Setup browser
+
+## Open Trendyol and close pop-up
+* Navigate to Trendyol
+* Close pop-up
+
+## Login
+* Click login button on navbar
+* Enter email and password
+* Submit login form
+* Verify login successful
--- src/test/java/OpenTrendyol.java
+++ src/test/java/OpenTrendyol.java
@@ -5,45 +5,72 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class OpenTrendyol {
 
     WebDriver driver;
 
-    @Step("OpenTrendyol")
-    public void openTrendyol() {
+    @Step("Setup browser")
+    public void setupBrowser() {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
-        WebDriver driver = new ChromeDriver(options);
-
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+    }
+
+    @Step("Navigate to Trendyol")
+    public void navigateToTrendyol() {
         driver.get("https://www.trendyol.com/");
+    }
+
+    @Step("Close pop-up")
+    public void closePopup() {
         WebElement closeButton = driver.findElement(By.xpath("//*[name()='svg' and @width='13px' and @height='13px']"));
         closeButton.click();
+    }
 
+    @Step("Click login button on navbar")
+    public void clickLoginButton() {
         WebElement navbarLoginButton = driver.findElement(By.xpath("//p[contains(text(),'Giriş Yap')]"));
         navbarLoginButton.click();
+    }
+
+    @Step("Enter email and password")
+    public void enterCredentials() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream file = new FileInputStream("config.properties");
+        properties.load(file);
+
+        String email = properties.getProperty("email");
+        String password = properties.getProperty("password");
 
         WebElement emailInput = driver.findElement(By.xpath("//input[@id='login-email']"));
-        emailInput.sendKeys("*");
+        emailInput.sendKeys(email);
 
         WebElement passwordInput = driver.findElement(By.xpath("//input[@id='login-password-input']"));
-        passwordInput.sendKeys("*");
+        passwordInput.sendKeys(password);
+    }
 
+    @Step("Submit login form")
+    public void submitLogin() {
         WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
         loginButton.click();
+    }
 
+    @Step("Verify login successful")
+    public void verifyLogin() {
         WebElement logo = driver.findElement(By.xpath("//img[@alt='Trendyol']"));
-
         if (logo.isDisplayed()) {
             System.out.println("Login successful!");
         } else {
             System.out.println("Login failed.");
         }
-
+        driver.quit();
     }
 }
